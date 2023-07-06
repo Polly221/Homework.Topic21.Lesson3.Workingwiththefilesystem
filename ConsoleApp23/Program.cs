903a8b6277c75031c14f2dfebf51972efa0f1095
Polinka <Polinka@DESKTOP-MHBJDTO>
2023-07-06 17:48:08 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+
+    string path = "C:\\Users\\Polinka\\Desktop\\ДЗ1";
+    Console.WriteLine("Текущая директория: " + path);
+
+while (true)
+{
+
+    Console.Write("Введите название папки для перехода(или 'выход' для завершения): ");
+    string nameDirectory = Console.ReadLine();
+
+    if (nameDirectory == "выход")
+    {
+        break;
+    }
+
+    string newDirectory = Path.Combine(path, nameDirectory);
+
+    if (Directory.Exists(newDirectory))
+    {
+        path = newDirectory;
+        Directory.SetCurrentDirectory(path);
+        Console.WriteLine("Текущая директория: " + path);
+        
+    }
+    else
+    {
+        Console.WriteLine("Папка не найдена.");
+    }
+}
